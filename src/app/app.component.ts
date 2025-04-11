7caf0aa5070c912c2340ecbabb49516151b60937
Your Name <you@example.com>
2025-04-11 11:21:47 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResulttableComponent } from './resulttable/resulttable.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ResulttableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/resulttable/resulttable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resulttable',
+  imports: [],
+  templateUrl: './resulttable.component.html',
+  styleUrl: './resulttable.component.css'
+})
+export class ResulttableComponent {
+
+}
